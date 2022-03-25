fb44f13e5cd52d63ec3bf123910e09d46162e1a0
MSL <lming2094@gmail.com>
2022-03-26 01:30:55 +0800

    step 1 finished

---
+++ db.sql
@@ -0,0 +1,90 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 100420
+ Source Host           : localhost:3306
+ Source Schema         : multi_ip
+
+ Target Server Type    : MySQL
+ Target Server Version : 100420
+ File Encoding         : 65001
+
+ Date: 26/03/2022 01:27:00
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for clients
+-- ----------------------------
+DROP TABLE IF EXISTS `clients`;
+CREATE TABLE `clients`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `server_id` int NULL DEFAULT NULL,
+  `client_ip` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
+  `con_time` datetime NULL DEFAULT NULL,
+  `dis_time` datetime NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of clients
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for connect_status
+-- ----------------------------
+DROP TABLE IF EXISTS `connect_status`;
+CREATE TABLE `connect_status`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `server_id` int NULL DEFAULT NULL,
+  `con_time` datetime NULL DEFAULT NULL,
+  `dis_time` datetime NULL DEFAULT NULL,
+  `status` int NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of connect_status
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for servers
+-- ----------------------------
+DROP TABLE IF EXISTS `servers`;
+CREATE TABLE `servers`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `ip` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
+  `port` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 23 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of servers
+-- ----------------------------
+INSERT INTO `servers` VALUES (1, '127.0.0.1', '4000');
+INSERT INTO `servers` VALUES (17, '127.0.0.1', '5000');
+INSERT INTO `servers` VALUES (22, '127.0.0.1', '1000');
+
+-- ----------------------------
+-- Procedure structure for newServerAdd
+-- ----------------------------
+DROP PROCEDURE IF EXISTS `newServerAdd`;
+delimiter ;;
+CREATE PROCEDURE `newServerAdd`(IN ip_address VARCHAR(255),
+	IN port_number VARCHAR(255))
+BEGIN
+	IF( SELECT EXISTS ( SELECT * FROM servers WHERE ip=ip_address and port=port_number )) THEN
+		SELECT 'EXISTS';
+	ELSE
+		INSERT INTO servers (ip, port)
+			VALUES (ip_address, port_number);
+	END IF;
+END
+;;
+delimiter ;
+
+SET FOREIGN_KEY_CHECKS = 1;
--- server.js
+++ server.js
@@ -2,73 +2,180 @@ const express = require('express')
 const app = express()
 const port = 3000
 const path = require('path');
-const { iotDeviceListnerBuilder } = require('./iot-device-listener-builder');
-const bodyParser = require('body-parser')
+const openProtocol = require('node-open-protocol');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+// Create DB connection
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "multi_ip",
+});
+
+db.connect(function(err) {
+    if (err) throw err;
+    console.log("DB Connected!");
+});
 
 app.set('views', __dirname);
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json({ type: 'application/*+json' }))
 
-let server_data = [{idx: 1, controllerIP: '127.0.0.1', controllerPORT: 3000}]
-let createdIpIntances = [];
+let serverData = [];
 
-exports.livetightening = (req, res) => {
-    _livetightening(res, req);
-};
+function updateServerStatus(ip, port, check_status) {
+    var checkDupServer = 0;
+    for (var i = 0; i < serverData.length; i++){
+        if (serverData[i].ip == ip && serverData[i].port == port){
+            serverData[i].status = check_status;
+            checkDupServer = 1
+            break;
+        }
+    }
+    if (serverData.length == 0 || checkDupServer == 0) {
+        serverData.push({ip: ip, port: port, status: check_status});
+    }
+}
 
-const _livetightening = (req, res) => {
-    server_data.forEach(ip => {
-        try {
-            console.log(`creating connection for ${ip.controllerIP}:${ip.controllerPORT}`)
-            let idlb = new iotDeviceListnerBuilder(ip.controllerPORT, ip.controllerIP);
-            let idl = idlb.build();
-            createdIpIntances.push(idl);
-        }
-        catch (err) {
-            console.error(`Error in making connection for ${ip.controllerIP}:${ip.controllerPORT}`, err);
-        }
+function _liveTightening(ip, port, remove_server=0) {
+    let op = openProtocol.createClient(port, ip, () => {
+        console.log("Connected!");
+
+        op.subscribe("lastTightening", (err, data) => {
+            if (err) {
+                updateServerStatus(ip, port, 1);
+                return console.log("Error on Subscribe", err);
+            }
+
+            startTightening(1, "ASDEDCUHBG34563EDFRCVGFR6");
+        });
     });
 
-    return createdIpIntances;
-};
+    op.on("error", (error) => {
+        updateServerStatus(ip, port, 1);
+        console.log("Error on OpenProtocol", error);
+    });
 
-_livetightening();
+    op.on("lastTightening", (midData) => {
+        console.log("Tightening received!", JSON.stringify(midData));
+    });
+
+    function startTightening(parameterSetID, numberVIN) {
+        // --> Abort Job --> Select Pset --> Set VehicleId --> Disable Tool --> Enable Tool
+
+        op.command("abortJob", (err) => {
+            if (err) {
+                updateServerStatus(ip, port, 1);
+                return console.log("Fail on abortJob", err);
+            }
+
+            op.command("selectPset", { payload: { parameterSetID } }, (err) => {
+
+                if (err) {
+                    updateServerStatus(ip, port, 1);
+                    return console.log("Fail on selectPset", err);
+                }
+
+                op.command("vinDownload", { payload: { numberVIN } }, (err) => {
+
+                    if (err) {
+                        updateServerStatus(ip, port, 1);
+                        return console.log("Fail on vinDownload", err);
+                    }
+
+                    op.command("disableTool", (err, data) => {
+
+                        if (err) {
+                            updateServerStatus(ip, port, 1);
+                            return console.log("Fail on disableTool", err);
+                        }
+
+                        op.command("enableTool", (err, data) => {
+
+                            if (err) {
+                                updateServerStatus(ip, port, 1);
+                                return console.log("Fail on enableTool", err);
+                            }
+
+                            console.log("waiting for the operator to tighten");
+                        });
+                    });
+                });
+            });
+        });
+    }
+    if (remove_server == 1){
+        updateServerStatus(ip, port, 1);
+    }
+    else {
+        updateServerStatus(ip, port, 0);
+    }
+}
 
 app.get('/', function(req, res) {
-    console.log(server_data)
-    res.render(path.join(__dirname, 'index.ejs'), { serverData: server_data })
+    let sql = `select * from servers`;
+    db.query(sql, function (err, results) {
+        if (err) {
+            throw err;
+        }
+        for (var i = 0; i < results.length; i ++){
+            _liveTightening(results[i].ip, parseInt(results[i].port));
+        }
+        console.log(serverData);
+        res.render(path.join(__dirname, 'index.ejs'), { serverData: results });
+    });
+});
+
+app.post('/server_status_check', function (req, res) {
+    var serverExists = 0;
+    var checkStatus = 0;
+    for (var i = 0; i < serverData.length; i++){
+        if (serverData[i].ip == req.body.ip && serverData[i].port == parseInt(req.body.port)){
+            serverExists = 1;
+            checkStatus = serverData[i].status;
+            break;
+        }
+    }
+    // console.log("Check Request", req.body.ip, req.body.port);
+    if (serverExists == 1 && checkStatus == 0) {
+        res.end('Connected');
+    }
+    else {
+        res.end('Not Connected');
+    }
 });
 
 app.post('/server_add', function (req, res) {
-    console.log(req.body);
-    server_data.push({ idx: server_data.length + 1, controllerIP: req.body.ip_address, controllerPORT: req.body.port_number});
-    let new_server_info = new iotDeviceListnerBuilder(req.body.ip_address, req.body.ip_address);
-    let new_server = new_server_info.build();
-    createdIpIntances.push(new_server);
-    res.redirect('/');
+    let sql = `call newServerAdd(?, ?)`;
+    let post = [req.body.ip_address, req.body.port_number];
+    db.query(sql, post,function (err, result) {
+        if (err) {
+            res.redirect('/');
+            throw err;
+        }
+        if (result[0] == undefined) {
+            console.log(`creating connection for ${req.body.ip_address}:${req.body.port_number}`)
+            _liveTightening(req.body.ip_address, parseInt(req.body.port_number));
+        }
+        console.log(serverData);
+        res.redirect('/');
+    });
 });
 
 app.post('/server_remove', function (req, res) {
-    for (var i = 0; i < server_data.length; i++) {
-        if (server_data[i].idx == req.body.idx)
-        {
-            for (var j = 0; j < createdIpIntances.length; j++) {
-                if (createdIpIntances[j].controllerIp == server_data[i].controllerIP && createdIpIntances[j].controllerPort == server_data[i].controllerPORT){
-                    createdIpIntances[j].unsubscribe("lastTightening", (err, data) => {
-                        if (err) {
-                            console.log("Error on unsubscribing to ", createdIpIntances[j].controllerIp, createdIpIntances[j].controllerPort, err);
-                            return;
-                        }
-
-                        console.log("Unsubscribed to ", createdIpIntances[j].controllerIp, createdIpIntances[j].controllerPort);
-                    });
-                }
-            }
-            server_data.splice(i , 1);
+    let sql = `delete from servers where ip='${req.body.ip_address}' and port='${req.body.port_number}'`;
+    db.query(sql, function (err) {
+        if (err) {
+            res.redirect('/');
+            throw err;
         }
-    }
-    res.redirect('/');
+        _liveTightening(req.body.ip_address, parseInt(req.body.port_number), 1);
+        console.log(serverData);
+        res.redirect('/');
+    });
 });
 
 app.listen(port, function () {
